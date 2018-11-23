eaeb98d7bca80b5cf1e7469a2537accb2dee5e3d
Mark Hoekstra <m.a.hoekstra@outlook.com>
2018-11-23 14:01:47 +0100

    updated

--- server.js
+++ server.js
@@ -3,61 +3,54 @@ const hbs = require('hbs');
 const fs = require('fs');
 
 const port = process.env.PORT || 3000;
-
-const app = express();
+var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
-app.set("view engine", "hbs");
+app.set('view engine', 'hbs');
 
 app.use((req, res, next) => {
-    const now = new Date().toString();
-    const log = `${now}: ${req.method} ${req.url}`;
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
 
-    console.log(log);
-    fs.appendFile('server.log', log + '\n', (err) => {
-        if (err) {
-            console.log('Unable to append to server.log.')
-        }
-    });
-    next();
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
 });
 
 // app.use((req, res, next) => {
-//     res.render('maintenance.hbs');
+//   res.render('maintenance.hbs');
 // });
 
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
-    return new Date().getFullYear()
+  return new Date().getFullYear();
 });
 
 hbs.registerHelper('screamIt', (text) => {
-    return text.toUpperCase();
+  return text.toUpperCase();
 });
 
 app.get('/', (req, res) => {
-    //res.send("<h1>Hello Express!</h1>");
-    res.render('home.hbs', {
-        pageTitle: 'Home Page',
-        welcomeMessage: "Hello, welcome to this website!",
-    })
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  });
 });
 
 app.get('/about', (req, res) => {
-    //res.send("<h1>Hello Express!</h1>");
- res.render('about.hbs', {
-     pageTitle: 'About Page',
- })
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
 });
 
+// /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
-    //res.send("<h1>Hello Express!</h1>");
-    res.send({
-        errorMessage: "Something didn't work as expected"
-    })
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Server is up on port ${port}`);
+  console.log(`Server is up on port ${port}`);
 });
